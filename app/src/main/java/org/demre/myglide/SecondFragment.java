2be95330aace85a960136b92b609f584ffe1847f
hipolito <huaylla7@hotmail.com>
2023-07-11 09:03:09 -0400

    Fragmentos Creados.

---
+++ app/src/main/java/org/demre/myglide/SecondFragment.java
@@ -0,0 +1,20 @@
+package org.demre.myglide;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import org.demre.myglide.databinding.FragmentFirstBinding;
+
+public class SecondFragment extends Fragment {
+
+    FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
